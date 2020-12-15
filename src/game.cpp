04d651d0d54e8137971ee65a9051caa1750ee6f1
S4kyt <jerikolo1@gmail.com>
2020-12-16 00:08:28 +0100

    extended ASCII values bug i found

--- src/game.cpp
+++ src/game.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 #include <thread>
 
+#include <Windows.h>
+
 #include "Game.h"
 #include "rand.h"
 
 struct Gamemap {
 public:
-	static const int width = 20;
-	static const int height = 40;
-	char boardArray[width][height]{};
+	static const int width = 3; //20;
+	static const int height = 3; //40;
+	// unsigned char so we are able to access the extended ASCII table and have cute "graphics"
+	unsigned char boardArray[width][height]{};
 };
 
 struct Node {
@@ -23,21 +26,50 @@ void Game::init() {
 	// draw game map
 	for (int x = 0; x < map->width; x++) {
 		for (int y = 0; y < map->height; y++) {
-			map->boardArray[x][y] = '.';
-			// TODO: other sides
-			// draw upper line
-			for (int x = 0; x < map->width / 20; x++) {
-				map->boardArray[x][y] = '_';
+			map->boardArray[x][y] = '.';			
+			// BOX:
+			// top side
+			if (x < map->width / map->width) {
+				map->boardArray[x][y] = 205;
 			}
-			// draw left line
-			for (int y = 0; y < map->height / 40; y++) {
-				map->boardArray[x][y] = '|';
+			// bottom side
+			if (x == map->width -1 )
+			{
+				map->boardArray[x][y] = 205;
 			}
-			// top left corner
-			for (int x = 0; x < map->width / 20; x++) {
-				for (int y = 0; y < map->height / 40; y++) {
-					map->boardArray[x][y] = ' ';
-				}
+			// left side
+			if (y < map->height / map->height) {
+				map->boardArray[x][y] = 186;
+			}
+			// right side
+			if (y == map->height - 1)
+			{
+				map->boardArray[x][y] = 186;
+			}
+
+			// CORNERS:
+			// top-left
+			if (x < map->width / map->width && y < map->height / map->height) {
+				map->boardArray[x][y] = 201;
+			}
+			// top-right
+			if (x < map->width / map->width && y == map->height - 1) {
+				map->boardArray[x][y] = 187;
+			}
+			// bottom-left
+			if (x == map->width - 1 && y < map->height / map->height) {
+				map->boardArray[x][y] = 200;
+			}
+			// bottom-right
+			if (x == map->width - 1 && y == map->height - 1) {
+				map->boardArray[x][y] = 188;
+			}
+
+
+			// SNAKE:
+			// snake head spawn
+			if (x == map->width / 2 && y == map->height / 2) {
+				map->boardArray[x][y] = 'X';
 			}
 		}
 	}
@@ -52,7 +84,20 @@ void Game::init() {
 
 		char apple = map->boardArray[widthValue][heightValue];
 
-		if (apple != '_' && apple != '|' && apple != ' ' && apple != '#' && apple != 'X') {
+		if (apple == '.') {
+			apple = map->boardArray[widthValue][heightValue];
+		}
+
+
+		// FIXME: collision is now all whacky because of the extended ASCII table values, unsure why this isn't working!
+		if (apple != 'X' // this is a clear example that ascii number table wont be a solution here, because instead of 'XYZ' number, the ASCII character is recorded as something else like 'Í' in this case!
+			&& apple != 186  //'º'
+			&& apple != 205  //'È'
+			&& apple != 201  //'¼'
+			&& apple != 200  //'»'
+			&& apple != 187  //'É'
+			&& apple != 188  //'X'
+			) {
 			map->boardArray[widthValue][heightValue] = 'O';
 			findingApplePos = false;
 		}
